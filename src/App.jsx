17066c10e87a53d7cfaacec4022e8c947e7cb4ef
Bney28 <bney1@outlook.com>
2023-01-30 17:49:23 -0500

    feat: added login, logout and profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import Login from './component/Login'
+import Logout from './component/Logout'
+import Profile from './component/Profile'
 
 const App = () => {
+  const {isAuthenticated} = useAuth0()
   return (
-    <div>App</div>
+    <div className='App'>
+        <h1>Hello</h1>
+      {isAuthenticated ? <><Profile /> <Logout /> </> : <><Login /></>}
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Auth0Provider
+  domain='bney1.us.auth0.com'
+  clientId='4whStNBoiCv9b0pBHMoaiMpphVG5eUuP'
+  authorizationParams={{redirect_uri: window.location.origin}}>
     <App />
+  </Auth0Provider>
   
 );
 
